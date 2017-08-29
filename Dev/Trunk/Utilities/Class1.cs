612807fac2aa818e64a82aec3e7babe08da061a5
chaperonaXceed <31394685+chaperonaXceed@users.noreply.github.com>
2017-08-29 11:11:01 -0400

    Added Utilities project

---
+++ Dev/Trunk/Utilities/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+  public class ValueProvider
+  {
+    public static int ProvideValue( int value )
+    {
+      return value * 2;
+    }
+
+    public static int ProvideValue( int value, bool square = false )
+    {
+      if( square )
+        return value * value;
+
+      return ValueProvider.ProvideValue( value );
+    }
+  }
+}
